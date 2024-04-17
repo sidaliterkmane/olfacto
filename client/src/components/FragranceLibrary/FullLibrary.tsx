3447b6af0952b55a6104a04f303bc94077f73048
Sid Ali Terkmane <sidterkmane@hotmail.com>
2024-04-17 10:33:54 -0400

    added pagination to library

--- client/src/components/FragranceLibrary/FullLibrary.tsx
+++ client/src/components/FragranceLibrary/FullLibrary.tsx
@@ -14,12 +14,28 @@ interface FullLibraryProps {
 
 const FullLibrary: React.FC<FullLibraryProps> = ({ fragrances, toggleFragranceFavorite, isFragranceFavorite, showFragrancePage }) => {
     const [searchTerm, setSearchTerm] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(20); // Adjust if needed
 
     const filteredFragrances = fragrances.filter(fragrance => {
         return fragrance.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
             fragrance.brand.toLowerCase().includes(searchTerm.toLowerCase()) ||
             fragrance.type.toLowerCase().includes(searchTerm.toLowerCase());
     });
+
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = filteredFragrances.slice(indexOfFirstItem, indexOfLastItem);
+    const totalItems = filteredFragrances.length;
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+    const nextPage = () => {
+        if (currentPage < totalPages) setCurrentPage(currentPage + 1);
+    }
+
+    const prevPage = () => {
+        if (currentPage > 1) setCurrentPage(currentPage - 1);
+    }
 
     return (
         <div className='flex flex-col gap-[2rem] box-border h-[90%] w-[103%]'>
@@ -28,7 +44,7 @@ const FullLibrary: React.FC<FullLibraryProps> = ({ fragrances, toggleFragranceFa
 
                 <div className='filters mr-[35px] flex gap-[1rem]'>
                     <div className='flex items-center gap-2'>
-                        <IoSearch className='text-neutral-500' size={'20px'}/>
+                        <IoSearch className='text-neutral-500' size={'20px'} />
                         <input
                             type="text"
                             placeholder="Search fragrances..."
@@ -44,7 +60,7 @@ const FullLibrary: React.FC<FullLibraryProps> = ({ fragrances, toggleFragranceFa
             <div className='custom-scrollbar flex flex-col gap-[2rem] box-border overflow-hidden overflow-y-scroll p-2'>
 
                 <div className='w-full flex flex-wrap gap-5 '>
-                    {filteredFragrances.map(fragrance => (
+                    {currentItems.map(fragrance => (
                         <FragranceCard
                             key={fragrance._id}
                             fragranceId={fragrance._id}
@@ -57,6 +73,11 @@ const FullLibrary: React.FC<FullLibraryProps> = ({ fragrances, toggleFragranceFa
                             onCardClick={() => showFragrancePage(fragrance._id)}
                         />
                     ))}
+                </div>
+                <div className='w-full flex justify-center gap-5'>
+                    <button className='text-lg text-neutral-600 hover:text-black dark:text-neutral-300 transition dark:hover:text-white disabled:dark:hover:text-neutral-300 disabled:hover:text-neutral-600' onClick={prevPage} disabled={currentPage === 1}>← Prev</button>
+                    <p className='dark:text-white text-lg'>{currentPage}</p>
+                    <button className='text-lg text-neutral-600 hover:text-black dark:text-neutral-300 transition dark:hover:text-white disabled:dark:hover:text-neutral-300 disabled:hover:text-neutral-600' onClick={nextPage} disabled={currentPage === totalPages}>Next →</button>
                 </div>
             </div>
         </div>
--- client/src/pages/FragrancePage.tsx
+++ client/src/pages/FragrancePage.tsx
@@ -35,16 +35,21 @@ const FragrancePage: React.FC<FragrancePageProps> = ({ fragranceId, library, onT
 
     return (
         <div className="w-[103%] h-full bg-neutral-200 dark:bg-neutral-950 flex flex-col gap-[1rem] overflow-hidden overflow-y-scroll custom-scrollbar p-1">
-            <div className="w-full mt-[20px] pt-[50px] mb-[130px] pr-5 flex gap-[2rem] justify-between px-[1rem]">
+            <div className="w-full mt-[20px] pt-[50px] mb-[80px] pr-5 flex gap-[2rem] justify-between px-[1rem]">
                 <div className='w-[60%]  flex flex-col gap-[1rem] justify-between'>
-                    <h1 className='text-7xl font-bold dark:text-white'>{fragrance.name}</h1>
-                    <p className='font-light dark:text-white text-md '><span className='font-light text-md text-blue-700'>By</span> {fragrance.brand}</p>
+                    <div>
+                        <h1 className='text-7xl font-bold dark:text-white'>{fragrance.name}</h1>
+                        <p className='font-light dark:text-white text-md '><span className='font-light text-md text-blue-700'>By</span> {fragrance.brand}</p>
+                    </div>
 
-                    <p className='box-border p-[5px] rounded-lg w-fit text-xs border-neutral-300 border dark:border-neutral-700 text-neutral-600 dark:text-neutral-500 bg-neutral-200 dark:bg-neutral-800 shadow'>{fragrance.family}</p>
+                    <div>
+                        <p className='box-border p-[5px] rounded-lg w-fit text-xs border-neutral-300 border dark:border-neutral-700 text-neutral-600 dark:text-neutral-500 bg-neutral-200 dark:bg-neutral-800 shadow'>{fragrance.family}</p>
 
-                    <p className='mt-[2rem] text-neutral-500'>
-                        {fragrance.description}
-                    </p>
+                        <p className='mt-[2rem] text-neutral-500'>
+                            {fragrance.description}
+                        </p>
+                    </div>
+
 
                     <div className='mt-10'>
                         <h1 className='text-lg font-normal mb-2 dark:text-neutral-300'>Fragrance Notes <br /></h1>
@@ -56,50 +61,50 @@ const FragrancePage: React.FC<FragrancePageProps> = ({ fragranceId, library, onT
                             })}
                         </div>
                     </div>
-                    
+
                     <div className='flex gap-[1rem] mt-[2rem]'>
-                    <button
-                        className="w-fit brightness-150 dark:brightness-100 p-[2px] rounded-lg bg-gradient-to-br from-blue-800 via-blue-600 to-blue-800 shadow-md"
-                    >
-                        <div
-                            className="px-6 py-2 backdrop-blur-xl bg-black/80 rounded-md font-semibold w-full h-full hover:brightness-125"
+                        <button
+                            className="w-fit brightness-150 dark:brightness-100 p-[2px] rounded-lg bg-gradient-to-br from-blue-800 via-blue-600 to-blue-800 shadow-md"
                         >
                             <div
-                                className="flex text-blue-600 gap-1"
+                                className="px-6 py-2 backdrop-blur-xl bg-black/80 rounded-md font-semibold w-full h-full hover:brightness-125"
                             >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.8"
-                                    className="w-6 h-6 stroke-blue-600"
+                                <div
+                                    className="flex text-blue-600 gap-1"
                                 >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
-                                    ></path>
-                                </svg>
-                                Get it now
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth="1.8"
+                                        className="w-6 h-6 stroke-blue-600"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+                                        ></path>
+                                    </svg>
+                                    Where to get it ?
+                                </div>
                             </div>
-                        </div>
-                    </button>
+                        </button>
 
-                    <button className='' onClick={() => onToggleFavorite(fragranceId, isFavorite(fragranceId))}>
-                        {heart}
-                    </button>
+                        <button className='' onClick={() => onToggleFavorite(fragranceId, isFavorite(fragranceId))}>
+                            {heart}
+                        </button>
                     </div>
-                    
+
 
 
                 </div>
 
                 <div className='w-[40%]'>
-                    <img className='object-cover w-full h-[700px] rounded-lg' src={fragrance.image} alt={fragrance.name} />
+                    <img className='object-cover w-full h-[700px] rounded  border border-neutral-300 dark:border-neutral-900' src={fragrance.image} alt={fragrance.name} />
                 </div>
             </div>
 
-            <div className='pr-5 border-t border-neutral-300 dark:border-neutral-700 pt-10'>
+            <div className='pr-5 border-t border-neutral-300 dark:border-neutral-700 pt-8'>
                 {library}
             </div>
 
